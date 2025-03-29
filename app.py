b7c859e2a7905f7068e7c267e454f442838aaf60
LeHuy <quanghuymang@gmail.com>
2025-03-29 18:24:50 +0700

    Update DB connection to FreeSQLDatabase

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from io import BytesIO
 from flask import send_file
 import pandas as pd
 from flask import Response
+import os  # thêm dòng này ở đầu file nếu chưa có
 
 app = Flask(__name__)
 
@@ -18,8 +19,9 @@ CORS(app)
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/stock_predictor'
 
 #db online 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql12770258:LsK7Hvu3nm@sql12.freesqldatabase.com/sql12770258'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://sql12770258:LsK7Hvu3nm@sql12.freesqldatabase.com/sql12770258'
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
